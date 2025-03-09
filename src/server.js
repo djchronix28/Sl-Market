e52dc744ed23f2cb1b3bc44cf889a85723ad6d11
djchronix28 <123938214+djchronix28@users.noreply.github.com>
2025-03-09 17:59:59 +0000

    Update Frontend server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://backend-vg1f.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://backend-vg1f.onrender.com/";
