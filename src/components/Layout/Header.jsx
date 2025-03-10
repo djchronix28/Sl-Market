42f4086687f8cd1de4c7760fa1b4633352f5acef
djchronix28 <123938214+djchronix28@users.noreply.github.com>
2025-03-10 11:43:17 +0000

    Update Header.jsx

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -234,7 +234,7 @@ const Header = ({ activeHeading }) => {
               onClick={() => setOpen(true)}
             />
           </div>
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
           <Link href="/">
             <img
               src="/SL_MARKET.png"
