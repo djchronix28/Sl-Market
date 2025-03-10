d5c9819a15397f90fc06916112f79c87a4c79efe
djchronix28 <123938214+djchronix28@users.noreply.github.com>
2025-03-10 14:29:52 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://backend-2-tmpp.onrender.com/api/v2";
+export const server = "https://backend-hazd.onrender.com/api/v2";
 
 export const backend_url = "http://localhost:8000/";
